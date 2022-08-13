a023c0849b901ced711bbd62c004dbf6d760820b
Sumit <sumitnaveen029@gmail.com>
2022-08-13 21:06:53 +0530

    Upgraded the UI for the App

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.tiptime
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.TextView
 import com.example.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
 import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
@@ -16,10 +21,30 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.calculateButton.setOnClickListener{ calculateTip() }
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode)
+        }
     }
 
-    fun calculateTip() {
-        val stringData = binding.costOfService.text.toString()
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
+    }
+
+    private fun calculateTip() {
+        val stringData = binding.costOfServiceEditText.text.toString()
+        if(stringData.isEmpty()){
+            binding.costOfService.hint = "Please Enter Cost"
+            var tip = 0
+            val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+            binding.result.text = getString(R.string.tip_amount, formattedTip)
+            return
+        }
         val cost = stringData.toDouble()
 
         val tipPercent = when(binding.tip.checkedRadioButtonId){
@@ -31,11 +56,12 @@ class MainActivity : AppCompatActivity() {
         var tip = (tipPercent/100.00)*cost
         binding.yesNo.isChecked
 
-        val num = 1
         if(binding.yesNo.isChecked){
             tip = ceil(tip)
         }
 
-        binding.result.text = tip.toString()
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.result.text = getString(R.string.tip_amount, formattedTip)
+
     }
 }
